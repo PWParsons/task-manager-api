6741eaf5a29b5b0258c53e6b93169261b2d82e6a
Peter <peterw.parsons@gmail.com>
2019-08-27 22:33:38 +0200

    Added auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 /**
@@ -10,7 +11,8 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save()
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
@@ -22,28 +24,24 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch (e) {
     res.status(400).send()
   }
 })
 
 /**
- * Read users
+ * Read profile
  */
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send(e)
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
 })
 
 /**
  * Read user
  */
-router.get('/users/:id', async (req, res) => {
+router.get('/users/:id', auth, async (req, res) => {
   const _id = req.params.id
 
   try {
@@ -62,7 +60,7 @@ router.get('/users/:id', async (req, res) => {
 /**
  * Update user
  */
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -90,7 +88,7 @@ router.patch('/users/:id', async (req, res) => {
 /**
  * Delete user
  */
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/:id', auth, async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id)
 
