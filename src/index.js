4d0f6a9ab7535001d0612bc718b09dda24e22fb1
Peter <peterw.parsons@gmail.com>
2019-08-28 10:17:00 +0200

    Authenticated task endpoint

--- src/index.js
+++ src/index.js
@@ -13,13 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`)
 })
-
-const User = require('./models/user')
-
-const main = async () => {
-  const user  = await User.findById('5d662ae42f7c3c364840d09c')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-}
-
-main()
