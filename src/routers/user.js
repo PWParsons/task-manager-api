005fa5b67aaaec3c4f55e7e31ebaff047bfe0fe7
Peter <peterw.parsons@gmail.com>
2019-08-28 23:55:48 +0200

    Added avatar endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
 
 /**
  * Create user
@@ -97,6 +98,41 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send(e)
   }
+})
+
+/**
+ * Upload user profile pic
+ */
+const upload = multer({
+  limits: {
+    fileSize: 1000000 // 1MB 
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'))
+    }
+
+    cb(undefined, true)
+  }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
+
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
+})
+
+/**
+ * Delete user avatar
+ */
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  
+  res.send()
 })
 
 module.exports = router
